fcc9bd33e7ca3a54cfadb93b89bee19f8b0c563e
Kristian Kinchev <83530894+kinchev@users.noreply.github.com>
2021-12-09 12:20:43 +0200

    Task MS project

---
+++ task-management-system/src/com/system/management/task/Main.java
@@ -0,0 +1,11 @@
+package com.system.management.task;
+
+import com.system.management.task.core.TaskEngineImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaskEngineImpl engine = new TaskEngineImpl();
+        engine.start();
+    }
+}
